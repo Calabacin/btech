2a4e4fd92a9ea6650aff205ecddfc8dd802f821b
Raúl <raul.garcia@mediamano.com>
2017-12-31 17:39:28 +0100

    Proyecto inicial

---
+++ src/main/java/eu/calabacin/juegos/btech/BtechApplication.java
@@ -0,0 +1,12 @@
+package eu.calabacin.juegos.btech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtechApplication.class, args);
+	}
+}
